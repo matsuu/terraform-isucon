537a4148a41daa04e941ad170499168a9745d361
MATSUU Takuto <matsuu@gmail.com>
2015-10-03 09:44:21 +0900

    fixed benchmark error issue. cleanup

--- gce/isucon5-qualifier/main.tf
+++ gce/isucon5-qualifier/main.tf
@@ -18,7 +18,7 @@ resource "google_compute_firewall" "ssh" {
     }
 
     source_ranges = ["0.0.0.0/0"]
-    target_tags = ["bench", "image"]
+    target_tags = ["${var.name}-bench", "${var.name}-image"]
 }
 
 resource "google_compute_firewall" "http" {
@@ -30,19 +30,7 @@ resource "google_compute_firewall" "http" {
     }
 
     source_ranges = ["0.0.0.0/0"]
-    target_tags = ["image"]
-}
-
-resource "google_compute_firewall" "mysql" {
-    name = "${var.name}-mysql"
-    network = "${google_compute_network.default.name}"
-    allow {
-        protocol = "tcp"
-        ports = ["3306"]
-    }
-
-    source_tags = ["bench"]
-    target_tags = ["image"]
+    target_tags = ["${var.name}-image"]
 }
 
 resource "google_compute_instance" "bench" {
@@ -52,7 +40,7 @@ resource "google_compute_instance" "bench" {
         image = "${var.bench_disk_image}"
     }
     zone = "${var.zone}"
-    tags = ["bench"]
+    tags = ["${var.name}-bench"]
     network_interface {
         network = "${google_compute_network.default.name}"
         access_config { }
@@ -62,23 +50,17 @@ resource "google_compute_instance" "bench" {
 set -e
 echo "Defaults:root !requiretty" >> /etc/sudoers
 apt-get update
-apt-get -y install ansible git
+apt-get -y install ansible git language-pack-ja
+update-locale LANG=ja_JP.UTF-8
 git clone https://github.com/isucon/isucon5-qualify.git
 (
     cd isucon5-qualify/gcp/bench/ansible/
     echo localhost ansible_connection=local > local
-    PYTHONUNBUFFERED=1 ansible-playbook -i local 00_devel.yml 01_isucon_base.yml 02_xbuild_part.yml 06_deploy_bench_tool.yml
+    PYTHONUNBUFFERED=1 ansible-playbook -i local *.yml
 )
 systemctl stop agent.ruby
 systemctl disable agent.ruby
 echo "/dev/sda1       /        ext4   defaults        0 0" > /etc/fstab
-sudo -u isucon bash -c '(source /home/isucon/.bashrc;echo $JAVA_HOME;cd /home/isucon/isucon5-qualify/bench/; gradle compileJava)' 
-#git clone https://github.com/matsuu/ansible-isucon.git
-#(
-#    cd ansible-isucon/isucon5-qualifier
-#    PYTHONUNBUFFERED=1 ansible-playbook -i local bench.yml
-#)
-#rm -rf ansible-isucon
 SCRIPT
 }
 
@@ -89,7 +71,7 @@ resource "google_compute_instance" "image" {
         image = "${var.image_disk_image}"
     }
     zone = "${var.zone}"
-    tags = ["image"]
+    tags = ["${var.name}-image"]
     network_interface {
         network = "${google_compute_network.default.name}"
         access_config { }
